240dc17f30a4358aa7ccaab84bd644f56cbea7dd
Lukas Gillsjö <lukasg@axis.com>
2021-09-24 17:16:36 +0200

    Add an empty daemon.config to avoid config collisions (#32)
    * Add an empty daemon.config to avoid config collisions

--- app/postinstallscript.sh
+++ app/postinstallscript.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# Move the daemon.json file into localdata folder
+if [ ! -e localdata/daemon.json ]
+then
+    mv empty_daemon.json localdata/daemon.json
+else
+    rm empty_daemon.json
+fi
+
 # Make sure containerd is started before dockerd and set PATH
 cat >> /etc/systemd/system/sdkdockerdwrapper.service << EOF
 [Unit]
