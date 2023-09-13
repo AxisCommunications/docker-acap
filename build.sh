f24b4f4e9fbcfc13d3bae0c688cac08e25550c37
Joakim Roubert <joakim.roubert@axis.com>
2023-09-13 16:27:22 +0200

    build.sh: Stop at errors and/or unset parameters (#99)
    * build.sh: Stop at errors and/or unset parameters
    
    Unless we stop at errors, the script will keep going on with new
    commands even if the previous ones fail. It should fail early to make
    the build process more solid and comprehensible.
    
    Also fail at unset parameters, and remove superfluous quotes at variable
    assignment.
    
    Change-Id: Id26abd68d51ec6aaeef20779116f375688b98c69
    Signed-off-by: Joakim Roubert <joakimr@axis.com>
    
    * build.sh: Handle empty PROXY variables
    
    Signed-off-by: Joakim Roubert <joakim.roubert@axis.com>
    
    ---------
    
    Signed-off-by: Joakim Roubert <joakimr@axis.com>
    Signed-off-by: Joakim Roubert <joakim.roubert@axis.com>

--- build.sh
+++ build.sh
@@ -1,29 +1,29 @@
-#!/bin/sh
-case "$1" in
+#!/bin/sh -eu
+case "${1:-}" in
     armv7hf|aarch64)
        ;;
     *)
        # error
-       echo "Invalid argument '$1', valid arguments are armv7hf or aarch64"
+       echo "Invalid argument '${1:-}', valid arguments are armv7hf or aarch64"
        exit 1
        ;;
 esac
 
 dockerdtag=dockerd:1.0
-imagetag="${2:-docker-acap:1.0}"
+imagetag=${2:-docker-acap:1.0}
 dockerdname=dockerd_name
 
 # First we build and copy out dockerd
 docker buildx build --build-arg ACAPARCH="$1" \
-             --build-arg HTTP_PROXY="$HTTP_PROXY" \
-             --build-arg HTTPS_PROXY="$HTTPS_PROXY" \
+             --build-arg HTTP_PROXY="${HTTP_PROXY:-}" \
+             --build-arg HTTPS_PROXY="${HTTPS_PROXY:-}" \
              --tag $dockerdtag \
              --no-cache \
              --file Dockerfile.dockerd .
 
 docker run -v /var/run/docker.sock:/var/run/docker.sock \
-           --env HTTP_PROXY="$HTTP_PROXY" \
-           --env HTTPS_PROXY="$HTTPS_PROXY" \
+           --env HTTP_PROXY="${HTTP_PROXY:-}" \
+           --env HTTPS_PROXY="${HTTPS_PROXY:-}" \
            --name $dockerdname \
            $dockerdtag
 
@@ -34,8 +34,8 @@ docker rm $dockerdname
 
 # Now build and copy out the acap
 docker buildx build --build-arg ACAPARCH="$1" \
-             --build-arg HTTP_PROXY="$HTTP_PROXY" \
-             --build-arg HTTPS_PROXY="$HTTPS_PROXY" \
+             --build-arg HTTP_PROXY="${HTTP_PROXY:-}" \
+             --build-arg HTTPS_PROXY="${HTTPS_PROXY:-}" \
              --file Dockerfile.acap \
              --no-cache \
              --tag "$imagetag" . 
