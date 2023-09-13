582b4b5804e5883d4e8302934fc45ccc0ab4b821
Joakim Roubert <joakim.roubert@axis.com>
2023-09-13 14:22:18 +0200

    build.sh: Handle empty PROXY variables
    Signed-off-by: Joakim Roubert <joakim.roubert@axis.com>

--- build.sh
+++ build.sh
@@ -15,15 +15,15 @@ dockerdname=dockerd_name
 
 # First we build and copy out dockerd
 docker buildx build --build-arg ACAPARCH="$1" \
-             --build-arg HTTP_PROXY="$HTTP_PROXY" \
-             --build-arg HTTPS_PROXY="$HTTPS_PROXY" \
+             --build-arg HTTP_PROXY="${HTTP_PROXY:-}" \
+             --build-arg HTTPS_PROXY="${HTTPS_PROXY:-}" \
              --tag $dockerdtag \
              --no-cache \
              --file Dockerfile.dockerd .
 
 docker run -v /var/run/docker.sock:/var/run/docker.sock \
-           --env HTTP_PROXY="$HTTP_PROXY" \
-           --env HTTPS_PROXY="$HTTPS_PROXY" \
+           --env HTTP_PROXY="${HTTP_PROXY:-}" \
+           --env HTTPS_PROXY="${HTTPS_PROXY:-}" \
            --name $dockerdname \
            $dockerdtag
 
@@ -34,8 +34,8 @@ docker rm $dockerdname
 
 # Now build and copy out the acap
 docker buildx build --build-arg ACAPARCH="$1" \
-             --build-arg HTTP_PROXY="$HTTP_PROXY" \
-             --build-arg HTTPS_PROXY="$HTTPS_PROXY" \
+             --build-arg HTTP_PROXY="${HTTP_PROXY:-}" \
+             --build-arg HTTPS_PROXY="${HTTPS_PROXY:-}" \
              --file Dockerfile.acap \
              --no-cache \
              --tag "$imagetag" . 
