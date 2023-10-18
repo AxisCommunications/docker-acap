4140822ec17fb7686c359fb97bcad7da6241a482
Madelen Andersson <madelen.andersson@axis.com>
2023-10-18 16:17:09 +0200

    Upgrade Docker version to 24.0 and native SDK to 1.10 (#100)
    * Updated docker version to 24.0 and removed build of dockerd since we no longer need to patch it
    * Updated native SDK to 1.10
    * Refactoring

--- app/dockerdwrapper.c
+++ app/dockerdwrapper.c
@@ -98,7 +98,7 @@ is_process_alive(int pid)
     // Report errors as dead.
     return false;
   } else if (return_pid == dockerd_process_pid) {
-    // Child is alread exited, so not alive.
+    // Child is already exited, so not alive.
     return false;
   }
   return true;
@@ -466,7 +466,7 @@ dockerd_process_exited_callback(__attribute__((unused)) GPid pid,
                                 __attribute__((unused)) gpointer user_data)
 {
   GError *error = NULL;
-  if (!g_spawn_check_exit_status(status, &error)) {
+  if (!g_spawn_check_wait_status(status, &error)) {
     syslog(LOG_ERR, "Dockerd process exited with error: %d", status);
     g_clear_error(&error);
 
