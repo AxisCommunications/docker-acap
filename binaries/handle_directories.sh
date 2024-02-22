45b3d47303f078b6666597a4d70b798f3a28c71a
Deepika Shanmugam <deepikas@axis.com>
2024-02-22 14:05:01 +0100

    Remove the script relates to cgroups

--- binaries/handle_directories.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-
-# Create /usr/run/<uid> on cgroups v1 system since user service will not run there
-# since script is run by root the required inputs are uid, user, group
-if [ -d /sys/fs/cgroup/unified ]; then
-    mkdir -p /run/user/"$1"
-    chown "$2":"$3" /run/user/"$1"
-fi
