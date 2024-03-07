1f42b2919998639b224bf0e8dfdaace64d2a0d44
Madelen Andersson <madelen.andersson@axis.com>
2024-03-07 12:07:04 +0100

    combined update of depenadbot recomendations (#131)
    * combined changes for depenadbot and other action updates

--- app/postinstallscript.sh
+++ app/postinstallscript.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
 # Move the daemon.json file into localdata folder
-if [ ! -e localdata/daemon.json ]
-then
-    mv empty_daemon.json localdata/daemon.json
+if [ ! -e localdata/daemon.json ]; then
+	mv empty_daemon.json localdata/daemon.json
 else
-    rm empty_daemon.json
+	rm empty_daemon.json
 fi
 
 # Make sure containerd is started before dockerd and set PATH
-cat >> /etc/systemd/system/sdkdockerdwrapper.service << EOF
+cat >>/etc/systemd/system/sdkdockerdwrapper.service <<EOF
 [Unit]
 BindsTo=containerd.service
 After=network-online.target containerd.service var-spool-storage-SD_DISK.mount
