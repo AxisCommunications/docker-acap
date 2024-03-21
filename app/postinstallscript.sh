7795497cedc64b68d5285af2665a0f7f5d34fc03
Madelen Andersson <madelen.andersson@axis.com>
2024-03-21 14:57:00 +0100

    remove containerd requirement (#134)

--- app/postinstallscript.sh
+++ app/postinstallscript.sh
@@ -7,11 +7,10 @@ else
 	rm empty_daemon.json
 fi
 
-# Make sure containerd is started before dockerd and set PATH
+# Set extended requirements and set PATH
 cat >>/etc/systemd/system/sdkdockerdwrapper.service <<EOF
 [Unit]
-BindsTo=containerd.service
-After=network-online.target containerd.service var-spool-storage-SD_DISK.mount
+After=network-online.target var-spool-storage-SD_DISK.mount
 Wants=network-online.target
 [Service]
 Environment=PATH=/usr/local/packages/dockerdwrapper:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin
