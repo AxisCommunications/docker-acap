0d485a8c29a60e399b9e387aae51befa163879e8
Mattias Axelsson <mattiaax@axis.com>
2024-03-22 08:44:03 +0100

    Older version of build.sh with tag docker-acap-with-compose

--- build.sh
+++ build.sh
@@ -1,51 +1,22 @@
-#!/bin/bash -eu
-
-imagetag="docker-acap:1.0"
-
-function exit_with_message {
-	echo "$1"
-	echo
-	echo "Usage: $0 ARCH [ --plain ] [ --cache ] [ --image-tag IMAGETAG ]"
-	echo
-	echo "ARCH must be 'armv7hf' or 'aarch64'"
-	echo "--plain will simplify the Docker progress bar."
-	echo "--cache will enable Docker's caching mechanism."
-	echo "--image-tag sets the supplied tag of the image. Default is $imagetag."
-	exit 1
-}
-
-arch="${1:-}"
-
-case "$arch" in
-armv7hf | aarch64) ;;
-*) exit_with_message "Invalid architecture '$arch'" ;;
+#!/bin/sh -eu
+case "${1:-}" in
+    armv7hf|aarch64)
+       ;;
+    *)
+       # error
+       echo "Invalid argument '${1:-}', valid arguments are armv7hf or aarch64"
+       exit 1
+       ;;
 esac
-shift
 
-progress_arg=
-cache_arg=--no-cache
+imagetag="${2:-docker-acap-with-compose:1.0}"
 
-while (($#)); do
-	case "$1" in
-	--plain) progress_arg="--progress plain" ;;
-	--cache) cache_arg= ;;
-	--image-tag)
-		shift
-		imagetag="$1"
-		;;
-	*) exit_with_message "Invalid argument '$1'" ;;
-	esac
-	shift
-done
+# Now build and copy out the acap
+docker buildx build --build-arg ACAPARCH="$1" \
+             --build-arg HTTP_PROXY="${HTTP_PROXY:-}" \
+             --build-arg HTTPS_PROXY="${HTTPS_PROXY:-}" \
+             --file Dockerfile \
+             --no-cache \
+             --tag "$imagetag" . 
 
-# Build and copy out the acap
-# shellcheck disable=SC2086
-docker buildx build --build-arg ACAPARCH="$arch" \
-	--build-arg HTTP_PROXY="${HTTP_PROXY:-}" \
-	--build-arg HTTPS_PROXY="${HTTPS_PROXY:-}" \
-	--file Dockerfile \
-	$progress_arg \
-	$cache_arg \
-	--tag "$imagetag" .
-
-docker cp "$(docker create "$imagetag")":/opt/app/ ./build-"$arch"
+docker cp "$(docker create "$imagetag")":/opt/app/ ./build-"$1"
