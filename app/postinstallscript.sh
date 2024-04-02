b7a5afa574f3865adf748029e12363fd7cd36b7e
Mattias Axelsson <mattiaax@axis.com>
2024-04-02 14:24:10 +0200

    Remove sdkdockerdwrapper.service modification from postinstall

--- app/postinstallscript.sh
+++ app/postinstallscript.sh
@@ -11,13 +11,3 @@ if [ ! -e localdata/daemon.json ]; then
 else
 	rm empty_daemon.json
 fi
-
-# Make sure containerd is started before dockerd and set PATH
-cat >>/etc/systemd/system/sdkdockerdwrapper.service <<EOF
-[Unit]
-BindsTo=containerd.service
-After=network-online.target containerd.service var-spool-storage-SD_DISK.mount
-Wants=network-online.target
-[Service]
-Environment=PATH=/usr/local/packages/dockerdwrapper:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin
-EOF
