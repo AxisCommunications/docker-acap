414a5b5625cf52370b208ea9165aed856af08470
Mattias Axelsson <mattiaax@axis.com>
2024-04-02 14:27:27 +0200

    Add comment "non-rooter user should be able to do this" to postinstall

--- app/postinstallscript.sh
+++ app/postinstallscript.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -e
 
+# *** non-root user should be able to do this ****
+
 if [ ! -e /usr/bin/containerd ]; then
 	logger -p user.warn "$0: Container support required to install application."
 	exit 77 # EX_NOPERM
