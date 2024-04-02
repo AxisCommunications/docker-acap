1ef989563536cb96fccaad0c909e8088101717c4
Mattias Axelsson <mattiaax@axis.com>
2024-04-02 14:26:35 +0200

    Shorten the code for moving daemon.json into localdata

--- app/postinstallscript.sh
+++ app/postinstallscript.sh
@@ -6,8 +6,5 @@ if [ ! -e /usr/bin/containerd ]; then
 fi
 
 # Move the daemon.json file into localdata folder
-if [ ! -e localdata/daemon.json ]; then
-	mv empty_daemon.json localdata/daemon.json
-else
-	rm empty_daemon.json
-fi
+mv -n empty_daemon.json localdata/daemon.json
+rm -f empty_daemon.json
