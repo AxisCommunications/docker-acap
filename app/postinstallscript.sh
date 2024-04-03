5eefb1f2f2cf00f1e802ab51b5f1f3c9f9590eb0
madelen-at-work <madelen-at-work@users.noreply.github.com>
2024-04-03 15:08:52 +0200

    Fail installation for non-container device

--- app/postinstallscript.sh
+++ app/postinstallscript.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/bin/sh -e
+
+if [ ! -e /usr/bin/containerd ]; then
+	logger -p user.warn "$0: Container support required to install application."
+	exit 77 # EX_NOPERM
+fi
 
 # Move the daemon.json file into localdata folder
 if [ ! -e localdata/daemon.json ]; then
