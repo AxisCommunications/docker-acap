4297ee70dea22ef2ae7ef3bdf83be08d3a5b0dd6
madelen-at-work <madelen-at-work@users.noreply.github.com>
2024-04-05 16:49:47 +0200

    don't change build.sh

--- build.sh
+++ build.sh
@@ -3,15 +3,15 @@
 imagetag="docker-acap:1.0"
 
 function exit_with_message {
-    echo "$1"
-    echo
-    echo "Usage: $0 ARCH [ --plain ] [ --cache ] [ --image-tag IMAGETAG ]"
-    echo
-    echo "ARCH must be 'armv7hf' or 'aarch64'"
-    echo "--plain will simplify the Docker progress bar."
-    echo "--cache will enable Docker's caching mechanism."
-    echo "--image-tag sets the supplied tag of the image. Default is $imagetag."
-    exit 1
+	echo "$1"
+	echo
+	echo "Usage: $0 ARCH [ --plain ] [ --cache ] [ --image-tag IMAGETAG ]"
+	echo
+	echo "ARCH must be 'armv7hf' or 'aarch64'"
+	echo "--plain will simplify the Docker progress bar."
+	echo "--cache will enable Docker's caching mechanism."
+	echo "--image-tag sets the supplied tag of the image. Default is $imagetag."
+	exit 1
 }
 
 arch="${1:-}"
@@ -26,25 +26,25 @@ progress_arg=
 cache_arg=--no-cache
 
 while (($#)); do
-    case "$1" in
-    --plain) progress_arg="--progress plain" ;;
-    --cache) cache_arg= ;;
-    --image-tag)
-        shift
-        imagetag="$1"
-        ;;
-    *) exit_with_message "Invalid argument '$1'" ;;
-    esac
-    shift
+	case "$1" in
+	--plain) progress_arg="--progress plain" ;;
+	--cache) cache_arg= ;;
+	--image-tag)
+		shift
+		imagetag="$1"
+		;;
+	*) exit_with_message "Invalid argument '$1'" ;;
+	esac
+	shift
 done
 
 # Build and copy out the acap
 # shellcheck disable=SC2086
 docker buildx build --build-arg ACAPARCH="$arch" \
-    --build-arg HTTP_PROXY="${HTTP_PROXY:-}" \
-    --build-arg HTTPS_PROXY="${HTTPS_PROXY:-}" \
-    --file Dockerfile \
-    $progress_arg \
-    $cache_arg \
-    --tag "$imagetag" \
-    --output build-"$arch" .
+	--build-arg HTTP_PROXY="${HTTP_PROXY:-}" \
+	--build-arg HTTPS_PROXY="${HTTPS_PROXY:-}" \
+	--file Dockerfile \
+	$progress_arg \
+	$cache_arg \
+	--tag "$imagetag" \
+	--output build-"$arch" .
