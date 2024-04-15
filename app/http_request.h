6ab5f57443ac2e65938e5337e2cecc8b34bb7fba
Mattias Axelsson <mattiaax@axis.com>
2024-04-15 07:56:33 +0200

    Restart dockerd after successful file upload

--- app/http_request.h
+++ app/http_request.h
@@ -1,4 +1,6 @@
 #pragma once
 
 // Callback function called from a thread by the FCGI server
-void http_request_callback(void* request_void_ptr, void* userdata);
+void http_request_callback(void* request_void_ptr, void* restart_dockerd_void_ptr);
+
+typedef void (*restart_dockerd_t)(void);
