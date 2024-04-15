d87ffbdca721e2c4361bf5088c8e6cb3454c9ee6
Mattias Axelsson <mattiaax@axis.com>
2024-04-15 07:56:37 +0200

    Allow dockerd to recover when uploading a new file

--- app/http_request.h
+++ app/http_request.h
@@ -1,6 +1,13 @@
 #pragma once
 
-// Callback function called from a thread by the FCGI server
-void http_request_callback(void* request_void_ptr, void* restart_dockerd_void_ptr);
+struct app_state;
 
-typedef void (*restart_dockerd_t)(void);
+typedef void (*restart_dockerd_t)(struct app_state*);
+
+struct restart_dockerd_context {
+    restart_dockerd_t restart_dockerd;
+    struct app_state* app_state;
+};
+
+// Callback function called from a thread by the FCGI server
+void http_request_callback(void* request_void_ptr, void* restart_dockerd_context_void_ptr);
